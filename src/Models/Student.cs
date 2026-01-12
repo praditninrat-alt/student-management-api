aa88744a03f52f09bc41b95b8f499cfb7043981b
praditninrat-alt <pradit.ninrat@gmail.com>
2026-01-12 15:30:18 +0700

    Add Student model with properties

---
+++ src/Models/Student.cs
@@ -0,0 +1,9 @@
+namespace StudentManagement.Api.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
